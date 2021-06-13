390830d1c1934c2d497f022461b3b5df33993caa
rohin <hobonobleXD@rohin.local>
2021-06-13 06:49:01 +0530

    downloader test

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app)
+const path = require('path');
+
 
 //Using direct file URL
-app.get('/',function(req,res){
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('download',function(req,res){
     res.download(__dirname +'/download/test.jpg','test.jpg');
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT,function(){
